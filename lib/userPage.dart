1e3d6977aa0b47c6b86121dba4b7c859c50f2806
chillit <121033253+chillit@users.noreply.github.com>
2023-04-13 07:55:32 +0600

    pretty userPage

--- lib/userPage.dart
+++ lib/userPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jobtake/main.dart';
+import 'package:jobtake/constans.dart';
 
 class UserPage extends StatelessWidget {
   final User user;
@@ -11,22 +12,191 @@ class UserPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text(user.fio),
-    ),
-    body:Center(
-      child: Column(
-        children: <Widget>[
-          Text(
-            user.fio,
-            style: const TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+        actions: [
+          InkWell(
+            onTap: () {
+              //send report to school management, in case if you want some changes to your profile
+            },
+            child: Container(
+              padding: EdgeInsets.only(right: kDefaultPadding / 2),
+              child: Row(
+                children: [
+                  Icon(Icons.report_gmailerrorred_outlined),
+                  kHalfWidthSizedBox,
+                  Text(
+                    'Report',
+                    style: Theme.of(context).textTheme.subtitle2,
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
-    )
-  );
-}*/
+      body: Container(
+        color: kOtherColor,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: kBottomBorderRadius,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: kSecondaryColor,
+                    backgroundImage:
+                    AssetImage('assets/images/student_profile.jpeg'),
+                  ),
+                  kWidthSizedBox,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Aisha Mirza',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text('Rating:1987 | Leaderboard:4',
+                          style: Theme.of(context).textTheme.subtitle2),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            sizedBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(
+                    title: 'Number', value: '2020-ASDF-2021'),
+                ProfileDetailRow(title: 'Rating', value: '2020-2021'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(title: 'Grade', value: 'X-II'),
+                ProfileDetailRow(title: 'Questions', value: '000126'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ProfileDetailRow(
+                    title: 'Date of Birth', value: '1 Aug, 2020'),
+                ProfileDetailRow(title: 'Leaderboard rating', value: '3 May 1998'),
+              ],
+            ),
+            sizedBox,
+            ProfileDetailColumn(
+              title: 'Email',
+              value: 'aisha12@gmail.com',
+            ),
+            ProfileDetailColumn(
+              title: 'School',
+              value: 'John Mirza',
+            ),
+            ProfileDetailColumn(
+              title: 'IDK',
+              value: 'Angelica Mirza',
+            ),
+            ProfileDetailColumn(
+              title: 'Phone Number',
+              value: '+923066666666',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileDetailRow extends StatelessWidget {
+  const ProfileDetailRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+  final String title;
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width/2,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                  color: kTextBlackColor,
+                  fontSize: 15,
+                ),
+              ),
+              kHalfSizedBox,
+              Text(value, style: Theme.of(context).textTheme.caption),
+              kHalfSizedBox,
+              SizedBox(
+                width: MediaQuery.of(context).size.width/3,
+                child: Divider(
+                  thickness: 1.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileDetailColumn extends StatelessWidget {
+  const ProfileDetailColumn(
+      {Key? key, required this.title, required this.value})
+      : super(key: key);
+  final String title;
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                  color: kTextBlackColor,
+                  fontSize: 15,
+                ),
+              ),
+              kHalfSizedBox,
+              Text(value, style: Theme.of(context).textTheme.caption),
+              kHalfSizedBox,
+              SizedBox(
+                width: MediaQuery.of(context).size.width/1.1,
+                child: Divider(
+                  thickness: 1.0,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
